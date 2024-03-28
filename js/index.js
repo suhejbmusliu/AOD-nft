6017453f0170223f60c01e6ada9cd849b4c0d8f0
suhejbmusliu <suhejbmusliu69@gmail.com>
2024-03-28 04:00:40 +0100

    update

--- js/index.js
+++ js/index.js
@@ -28,7 +28,31 @@
   });
 
  
- 
+  var loader = document.getElementById("preloader");
 
-
- 
+  // Show the preloader when the window loads
+  window.addEventListener("load", function(){
+      // Display the preloader
+      loader.style.display = "block";
+  
+      // Function to hide the preloader
+      function hidePreloader() {
+          loader.style.opacity = 0; // Fade out the preloader
+          setTimeout(function() {
+              loader.style.display = "none"; // Hide the preloader after fading out
+          }, 500); // Fading duration (0.5 seconds)
+      }
+  
+      // Hide the preloader after animation finishes
+      loader.addEventListener("animationend", function(event) {
+          if (event.animationName === "slideUp") {
+              hidePreloader();
+          }
+      });
+  
+      // Fallback for browsers that do not support CSS animations
+      setTimeout(function() {
+          hidePreloader();
+      }, 4500); // Fallback timeout (slightly longer than animation duration)
+  });
+  
